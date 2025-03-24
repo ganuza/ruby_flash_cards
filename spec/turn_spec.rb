b0c8f1c77eed62073e58569225cf086c4242db9d
Adrian Zabolitzki <adrianzab@gmail.com>
2025-03-24 16:25:12 -0600

    Add sad path turn exists

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -38,5 +38,13 @@ describe Turn do
 
     expect(turn.feedback).to eq("Correct!")
   end
+end
 
+describe Turn do
+  it 'exists' do
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
+
+    expect(turn).to be_instance_of(Turn)
+  end
 end
