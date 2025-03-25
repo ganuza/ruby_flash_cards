c4754abbf2b2e8ecd0880fb3be471552cf897930
Adrian Zabolitzki <adrianzab@gmail.com>
2025-03-25 11:40:09 -0600

    Add tests for category filter

--- lib/deck.rb
+++ lib/deck.rb
@@ -12,4 +12,16 @@ class Deck
   def count
     @cards.length
   end
+
+  def cards_in_category(category)
+    result = []
+    @cards.each do |card|
+      if card.category == category
+        result << card
+      end
+    end
+    result
+  end
+
+    
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -31,4 +31,22 @@ describe Deck do
     expect(deck.count).to eq(3)
   end
 
+  it 'can filter cards by category' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    stem_cards = deck.cards_in_category(:STEM)
+    expect(stem_cards).to eq([card_2, card_3])
+
+    geography_cards = deck.cards_in_category(:Geography)
+    expect(geography_cards).to eq([card_1])
+
+    pop_culture_cards = deck.cards_in_category(:Pop_Culture)
+    expect(pop_culture_cards).to eq([])
+  end
+
 end
