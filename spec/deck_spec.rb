444c6fe35f3fe2856ff7e1e2aac9c1d0ec28fda6
Adrian Zabolitzki <adrianzab@gmail.com>
2025-03-25 11:16:13 -0600

    Add test for creating an instance of Deck with multiple cards

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,4 +12,13 @@ describe Deck do
     expect(deck).to be_instance_of(Deck)
     expect(deck.cards).to eq([card_1])
   end
+
+  it 'can be initialized with multiple cards' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+
+    expect(deck.cards).to include(card_1, card_2, card_3)
+  end
 end
