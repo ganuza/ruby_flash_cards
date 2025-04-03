067a84d888bc4f52dcda745860f0180eead3f5e8
Adrian Zabolitzki <adrianzab@gmail.com>
2025-04-03 15:58:31 -0600

    Add start method to play game

--- lib/runner.rb
+++ lib/runner.rb
@@ -14,9 +14,41 @@ deck = Deck.new([card_1, card_2, card_3])
 # Create a round with the deck
 round = Round.new(deck)
 
+#########################################################
 # Start an interactive Pry session
-require 'pry'; binding.pry
+# require 'pry'; binding.pry  <-- uncomment this to debug
+#########################################################
 
-# This line won't execute until after you exit Pry
-puts "Thanks for playing!"
+def start(round)
+  puts "Welcome! You're playing with #{round.deck.cards.count} cards."
+  puts "--------------------------------------------------------"
 
+  while round.turns.count < round.deck.cards.count
+    current_card = round.current_card
+    puts "This is card #{round.turns.count + 1} out of #{round.deck.cards.count}."
+    puts "Question: #{current_card.question}"
+
+    guess = gets.chomp
+
+    turn = round.take_turn(guess)
+
+    puts turn.feedback
+    puts ""
+  end
+
+  puts "****** Game over! ******"
+  puts "You had #{round.number_correct} guesses out of #{round.turns.count} for a total score of #{round.percent_correct}%."
+
+  categories = []
+  round.deck.cards.each do |card|
+    if !categories.include?(card.category)
+      categories << card.category
+    end
+  end
+
+  categories.each do |category|
+    puts "#{category} - #{round.percent_correct_by_category(category)} % correct"
+  end
+end
+
+start(round)
